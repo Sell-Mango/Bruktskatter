d15b4fccf2b10402f4a844a251fcd8ed0d28eaf4
Marius <mariuror@hiof.no>
2025-10-09 22:23:28 +0200

    Created development environment
    Created based on react native expo template blank-typescript.
    in addition added packages: expo-router, react-native-safe-area-context, react-native-screens, expo-linking

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up index.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+        <Link href="#">
+            <Text>Open up index.js</Text>
+        </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
