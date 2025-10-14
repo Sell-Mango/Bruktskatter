703e866b259b75b482af9a6efa4b92fe565212bd
Marius <mariuror@hiof.no>
2025-10-14 15:59:40 +0200

    Added: More tabs and top icon for profile
    added two new tabs navigations and a button to reach profile in the header.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,19 @@
-import {Tabs} from "expo-router";
+import {router, Tabs} from "expo-router";
+import {Pressable, Text} from "react-native";
 
 export default function TabsLayout() {
+    const profileRight = () =>
+        (<Pressable onPress={() =>
+                router.push("/(tabs)/profile")
+            } style={{
+                margin: 5,
+                padding: 10,
+                borderStyle: "solid",
+                borderColor: "black",
+                borderWidth: 2,
+                borderRadius: 100,
+            }}><Text>profile</Text></Pressable>
+        )
     return(
         <Tabs screenOptions={{
             headerStyle: {
@@ -10,10 +23,13 @@ export default function TabsLayout() {
             headerTitleStyle: {
                 fontWeight: "bold",
             },
-            headerShown: true
+            headerShown: true,
         }}>
-            <Tabs.Screen name="frontpage" options={{title: "Utforsk"}}/>
-            <Tabs.Screen name="favorites" options={{title: "Skatter"}} />
+            <Tabs.Screen name="frontpage" options={{title: "Utforsk", headerRight: profileRight}}/>
+            <Tabs.Screen name="favorites" options={{title: "Skatter", headerRight: profileRight}} />
+            <Tabs.Screen name="feed" options={{title: "Feed", headerRight: profileRight}}/>
+            <Tabs.Screen name="my-market" options={{title: "Mitt marked", headerRight: profileRight}}/>
+            <Tabs.Screen name="profile" options={{title: "Profile", href: null}}/>
         </Tabs>
     )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,7 @@ export default function RootLayout() {
             headerShown: false,
         }}>
             <Stack.Screen name="index" options={{title: ""}}/>
-            <Stack.Screen name="frontpage" options={{title: "bruktskatter"}} />
-            <Stack.Screen name="(auth)/login" options={{title: ""}} />
+            <Stack.Screen name="(auth)" options={{title: ""}} />
         </Stack>
     )
 }
