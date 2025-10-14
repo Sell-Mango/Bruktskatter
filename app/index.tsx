ca8581c5c7107a7d3c2109991c5bee3f64e325c8
Marius <mariuror@hiof.no>
2025-10-14 23:45:52 +0200

    hotfix: removed interactive map from index again

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,11 @@
 import Welcome from "../features/welcomepage/view/Welcome";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 
-import InteractiveMap from '../features/interactive-map/InteractiveMap';
-
 export default function App() {
 
   return (
       <SafeAreaProvider>
           <Welcome/>
-          <InteractiveMap />
       </SafeAreaProvider>
   );
 }
