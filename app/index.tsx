5dfbd369bff111444979fb10c75402f7cbef835e
Marius <mariuror@hiof.no>
2025-10-14 22:18:57 +0200

    fix: fixed wrong import

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
 import Welcome from "../features/welcomepage/view/Welcome";
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 
-import type { Region } from 'react-native-maps';
-import { ShopMarker } from './interactive-map/types/shopMarker';
-import InteractiveMap from './interactive-map/InteractiveMap';
+import InteractiveMap from '@/features/interactive-map/InteractiveMap';
 
 export default function App() {
 
