690124008ed4cd471bfc8a1bdd862fc5da7809c4
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-11-16 21:47:02 +0100

    Changed: Native-maps to MapLibre with default tiles. Updated project dependencies

---
+++ features/interactive-map/view/InteractiveMap.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { getShops } from '../services/interactiveMapService';
+import { Suspense, useEffect, useState } from "react";
+
+import { ShopMarker } from '../types/shopMarker';
+import {MapView} from "@maplibre/maplibre-react-native";
+
+
+export default function InteractiveMap() {
+
+    const [markers, setMarkers] = useState<ShopMarker[]>([]);
+
+        useEffect(() => {
+          const fetchMarkers = async () => {
+            const locations = await getShops();
+            
+            const formattedLocations = locations.documents.map((marker) => ({
+              id: marker.$id, latitude: marker.location[0], longitude: marker.location[1], latitudeDelta: 0.05, longitudeDelta: 0.05, name: marker.name
+            }));
+            setMarkers(formattedLocations);
+          }
+          fetchMarkers();
+        }
+        ,[])
+
+    return (
+        <MapView style={{ flex: 1 }}/>
+
+       /* <Suspense fallback={<Text>Laster...</Text>}>
+
+        </Suspense> */
+
+    )
+}
+
+const styles = StyleSheet.create({
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+});
