8f49bb601ace5fd4667a8345dcc47ec86742347e
Kjell-Magne <kjell_m.larsen@yahoo.com>
2025-11-25 12:25:44 +0100

    Added: Gps icon. Chanegd: Wrapped frontpage in location provider. Changed: User location marker on map.

--- app/(drawer)/(tabs)/frontpage.tsx
+++ app/(drawer)/(tabs)/frontpage.tsx
@@ -4,17 +4,20 @@ import ShopList from "@/features/shopListView/view/ShopList";
 import FloatingButton from "@/features/shopListView/view/FloatingButton";
 import {useState} from "react";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
+import UserLocationProvider from "@/shared/context/UserLocationProvider";
 
 export default function frontpage() {
     const [mapView, setMapView] = useState<boolean>(true)
     return (
         <>
-            <View style={{flex: 1,backgroundColor: '#F6F6E9'}}>
-                {
-                    mapView ? <InteractiveMap/> : <ShopList/>
-                }
-                <FloatingButton mapView={mapView} setMapView={setMapView}/>
-            </View>
+            <UserLocationProvider>
+                <View style={{flex: 1,backgroundColor: '#F6F6E9'}}>
+                    {
+                        mapView ? <InteractiveMap/> : <ShopList/>
+                    }
+                    <FloatingButton mapView={mapView} setMapView={setMapView}/>
+                </View>
+            </UserLocationProvider>
         </>
     )
 }
