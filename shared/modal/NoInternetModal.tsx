1408aa220609d6fdea1e8e2a2f6bbc8030328a1d
Marius <mariuror@hiof.no>
2025-11-28 21:43:00 +0100

    Changed: modal now opens wifi settings for android
    the button on the modal takes the user to wifi settings on android, while general settings for ios as ios restricts opening a spesific page in settings app.

--- shared/modal/NoInternetModal.tsx
+++ shared/modal/NoInternetModal.tsx
@@ -1,13 +1,24 @@
-import {Modal, View, Text, StyleSheet} from "react-native";
+import {Modal, Platform, View} from "react-native";
 import {useNetInfo} from "@react-native-community/netinfo";
 import CustomPress from "@/shared/components/CustomPress";
 import HeadingText from "@/shared/components/HeadingText";
 import {openSettings} from "expo-linking"
 import {containerStyles} from "@/shared/stylesheets";
+import {ActivityAction, startActivityAsync} from "expo-intent-launcher";
 
 export default function NoInternetModal() {
     const netInfo = useNetInfo();
     const isOffline = netInfo.isConnected === false
+
+    const sendUserToSettings = async () => {
+        if (Platform.OS === "android") {
+            await startActivityAsync(
+                ActivityAction.WIFI_SETTINGS
+            )
+        }else {
+            await openSettings()
+        }
+    }
 
     return (
         <Modal
@@ -20,7 +31,7 @@ export default function NoInternetModal() {
                 <View style={containerStyles.modalContainer}>
                     <HeadingText heading={"Ingen internettforbindelse"} type={"h1"}/>
                     <HeadingText heading={"Vennligst koble til internett for å fortsette"} type={"h2"}/>
-                    <CustomPress pressAction={openSettings}><HeadingText heading={"Åpne instillinger"} type={"h2"} color={"#fff"}/></CustomPress>
+                    <CustomPress pressAction={sendUserToSettings}><HeadingText heading={"Åpne instillinger"} type={"h2"} color={"#fff"}/></CustomPress>
                 </View>
             </View>
         </Modal>
